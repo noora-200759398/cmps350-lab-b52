dd363350ba8187cd549ab4f61edbae9fdc418f95
Georges <georges.younes@gmail.com>
2022-04-11 00:00:53 +0300

    add files for lab11

---
+++ labs/lab11/base/project9.js
@@ -0,0 +1,9 @@
+/*
+1.	In project9.js :  Rewrite the code you created in project7.js but this time switch to using the node-fetch library.
+File locations:
+data/course.json
+data/student.json
+data/course.json
+data/staff.json
+data/ceng-programs.json
+*/
